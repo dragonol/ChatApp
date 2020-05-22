e89916d1c5fbe627ab60e8eb60376e58f2d4a440
Huy Ha <huyhnc81@gmail.com>
2020-05-22 12:30:51 +0700

    working on register

--- backend/cmd/controllers/authenticatedHandlers.go
+++ backend/cmd/controllers/authenticatedHandlers.go
@@ -1,11 +1,15 @@
 package controllers
 
 import (
-	mongodb "../db"
 	"context"
+	"fmt"
+	"log"
+
+	mongodb "../db"
+	models "../models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"log"
+	"golang.org/x/crypto/bcrypt"
 )
 
 //Login handler for /api/login
@@ -18,21 +22,22 @@ func Login(c *gin.Context) {
 	}
 
 	collection := db.Collection("users")
-	var result bson.D
+	var result models.UserLogin
 	collection.FindOne(context.TODO(), bson.D{
 		{"email", c.PostForm("email")},
 		{"password", c.PostForm("password")},
 	}).Decode(&result)
 
-	if result != nil {
-		c.JSON(200, gin.H{
-			"status": "success",
-		})
-	} else {
-		c.JSON(200, gin.H{
-			"status": "fail",
-		})
-	}
+	fmt.Println(result)
+	// if result != nil {
+	// 	c.JSON(200, gin.H{
+	// 		"status": "success",
+	// 	})
+	// } else {
+	// 	c.JSON(200, gin.H{
+	// 		"status": "fail",
+	// 	})
+	// }
 }
 
 func Register(c *gin.Context) {
@@ -42,5 +47,12 @@ func Register(c *gin.Context) {
 		log.Fatal(err)
 		return
 	}
-	
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(c.PostForm("password")), 14)
+	record := models.UserLogin{c.PostForm("email"), passwordHash}
+	_, err = db.Collection("users").InsertOne(context.TODO(), record)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ backend/cmd/models/UserLogin.go
@@ -0,0 +1,6 @@
+package models
+
+type UserLogin struct{
+	Email string
+	Password []byte
+}
