28b35e080045d06d4f81de44c681139310e3537b
khang00 <dlqkhang2@gmail.com>
2020-06-13 20:11:41 +0700

    clean

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import router from './router'
-import loginPage from './views/Login.vue'
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(loginPage)
-}).$mount('#app')
